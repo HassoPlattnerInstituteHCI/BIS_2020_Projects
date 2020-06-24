bbf035b0b900cda413be7db682014bca6d83dc62
Ben Justus Bals <benbals@posteo.de>
2020-06-24 17:27:52 +0200

    make enemy move on given path

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 1.0f; 
+    // The enemy will patrol between these points;
+    public Vector3[] path;
+    // The part of the path the enemy is currently headed to
+    private int currentPathTargetIndex = 0;
+    
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.position == getCurrentTarget())
+        {
+            currentPathTargetIndex = (currentPathTargetIndex + 1) % path.Length;
+        }
+
+        moveTowardsTarget();
+    }
+
+    void moveTowardsTarget()
+    {
+        gameObject.transform.position =
+            Vector3.MoveTowards(gameObject.transform.position, getCurrentTarget(), Time.deltaTime * speed);
+    }
+
+    Vector3 getCurrentTarget()
+    {
+        return path[currentPathTargetIndex];
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Packages.Rider.Editor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed = 1.0f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        PantoMovement();
+    }
+    
+    void PantoMovement()
+        {
+            float rotation = GameObject
+                .Find("Panto")
+                .GetComponent<UpperHandle>()
+                .getRotation();
+            Vector3 direction = Quaternion.Euler(0, rotation, 0) * Vector3.forward;
+            playerRb.velocity = speed * direction;
+        }
+}
