70b3ddb5d1a3d78bc674e4add549281728db24f0
NikkelM <57323886+NikkelM@users.noreply.github.com>
2020-06-30 20:45:11 +0200

    Added Block Prefabs and SpawnManager

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public GameObject[] groups;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnNext();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void spawnNext() {
+
+    int i = Random.Range(0, groups.Length);
+
+    // Spawn Group at current Position
+    Instantiate(groups[i], transform.position, Quaternion.identity);
+    }
+}
