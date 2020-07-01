0bf39b90779915a1bbbd72c17d41e722002f4f85
Philipp Keese <philk.7438@gmail.com>
2020-07-01 16:30:12 +0200

    Add demo for closest point on path

--- Assets/MoveOnPath.cs
+++ Assets/MoveOnPath.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PathCreation;
 
 public class MoveOnPath : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public PathCreator pathCreator;
+    public LayerMask layerMask;
+    public float stepSize = 1;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+        {
+            Transform objectHit = hit.transform;
+
+            // Do something with the object that was hit by the raycast.
+            transform.position = GetClosestPoint(hit.point);
+            GetComponent<MeshRenderer>().enabled = true;
+        }
+        else
+        {
+            GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+
+    Vector3 GetClosestPoint(Vector3 point)
+    {
+        return pathCreator.path.GetClosestPointOnPath(point);
     }
 }
