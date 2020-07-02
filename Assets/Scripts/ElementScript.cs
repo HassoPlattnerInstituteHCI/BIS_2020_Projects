04b1357280ca8e7f7341933d30ad9fd2ee059986
Linus Hagemann <linus.hagemann@student.hpi.de>
2020-07-02 18:50:27 +0200

    add labels to elements

--- Assets/Scripts/ElementScript.cs
+++ Assets/Scripts/ElementScript.cs
@@ -7,6 +7,7 @@ public class ElementScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        GetComponentInChildren<TextMesh>().text = name;
     }
 
     // Update is called once per frame
