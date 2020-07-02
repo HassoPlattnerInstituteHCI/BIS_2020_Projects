c697c769e92b1e7edc7ffcf84eaf18a414d0e796
Philipp Keese <philk.7438@gmail.com>
2020-07-02 17:32:55 +0200

    make it handle follow path

---
+++ Assets/FollowMe.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMe : MonoBehaviour
+{
+    public LowerHandle handle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject panto = GameObject.FindGameObjectWithTag("Panto");
+        print(panto);
+        handle = panto.GetComponent<LowerHandle>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        handle.SwitchTo(gameObject, 0.2f);
+    }
+}
