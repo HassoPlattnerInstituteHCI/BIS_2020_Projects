37360175185f25e9f7668340586df8c38bb1b2a5
JulianEgbert <Julian.Egbert@student.hpi.de>
2020-07-03 12:54:55 +0200

    First Implementations

---
+++ Assets/BallScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("goal"))
+        {
+            //MISSING: Play goal sound
+            // Start next Level
+        }
+        else if (other.gameObject.CompareTag("water"))
+        {
+            // MISSING: Play Water drop sound
+            RestartLevel();
+        }
+    }
+
+    void RestartLevel()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
