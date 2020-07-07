4c18bd299b8b9e90bd2dc877540b2ac8f70e8670
yannis216 <yannisbeuke@googlemail.com>
2020-07-07 17:27:45 +0200

    Edited Starting Dialogs

--- Assets/Scripts/TelephoneSoundEffect.cs
+++ Assets/Scripts/TelephoneSoundEffect.cs
@@ -35,7 +35,7 @@ public class TelephoneSoundEffect : MonoBehaviour
 
     public void startPhoneTalks(int level){
         if(level == 1){
-            speechOut.Speak("Yo Claude, it's Johnny Zoo. Congratulations on breaking out of prison!");
+            speechOut.Speak("Yo Claude, it's Johnny Zoo. Congratulations on breaking out of prison! Welcome back to Downtown Anywhere City. Maaan. This city went crazy during the time you spent in prison. You may have some trouble navigating without being able to see. I will help you out. Grab the baseball bat that I have hidden here in the telephone booth. You can use it to spot objects around you. I will call you on the other telephone booth to see if that works for you.");
         }
     }
 
